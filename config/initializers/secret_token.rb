6be9223f3b74ae67638a214d500b1f3a6288c566
Gael <gael@step6.fr>
2013-01-12 14:21:59 +0100

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HidsUi::Application.config.secret_token = 'cde82fabaa2d22adc00c28cafa035a1198778abd3bb6fb8d539663e1c1c69c0979139b8962a8de66d1b22019b507da974eb06e25185863b5302ba85857dc793c'
